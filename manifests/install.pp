ec1d0d7ba97bd80af04ef7c98f13088cee1836c8
Jared Curtis <jared@shift-e.info>
2015-01-22 13:43:08 -0800

    We shouldn't install python-pip directly in the module
    Installing dependancies should be handled outside of the module to
    prevent duplicate package conflicts.

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,18 +7,13 @@ class diamond::install {
 
   if $diamond::install_from_pip {
     case $::osfamily {
-      RedHat: {
-        include epel
-        ensure_resource('package', 'python-pip', {'ensure' => 'present', 'before' => Package['diamond'], 'require' => Yumrepo['epel']})
-        ensure_resource('package', ['python-configobj','gcc','python-devel'], {'ensure' => 'present', 'before' => Package['diamond'], 'require' => Package['python-pip']})
-      }
-      /^(Debian|Ubuntu)$/: {
-        ensure_resource('package', ['python-pip','python-configobj','gcc','python-dev'], {'ensure' => 'present', 'before' => Package['diamond']})
-      }
       Solaris: {
         case $::operatingsystemrelease {
           '5.11': {
-            ensure_resource('package', ['pip','solarisstudio-122'], {'ensure' => 'present', 'before' => Package['diamond']})
+            package { 'solarisstudio-122':
+              ensure => present,
+              before => Package['diamond'],
+            }
             file { ['/ws', '/ws/on11update-tools', '/ws/on11update-tools/SUNWspro']: ensure => directory, }
             file { '/ws/on11update-tools/SUNWspro/sunstudio12.1':
               ensure  => symlink,
@@ -33,42 +28,41 @@ class diamond::install {
           }
         }
       }
-      default: { fail('Unrecognized operating system') }
     }
-  package {'diamond':
-    ensure   => present,
-    provider => pip,
-  }
-  if $::osfamily == 'Solaris' {
-    # This should eventually go upstream
-    file { '/lib/svc/method/diamond':
-      source  => 'puppet:///modules/diamond/solaris/method/diamond',
-      mode    => '0755',
-      owner   => 'root',
-      group   => 'bin',
-      require => Package['diamond'],
+    package { 'diamond':
+      ensure   => present,
+      provider => pip,
     }
-    file { '/lib/svc/manifest/network/diamond.xml':
-      source  => 'puppet:///modules/diamond/solaris/manifest/diamond.xml',
-      mode    => '0444',
-      owner   => 'root',
-      group   => 'sys',
-      require => [Package['diamond'],File['/lib/svc/method/diamond']],
+    if $::osfamily == 'Solaris' {
+      # This should eventually go upstream
+      file { '/lib/svc/method/diamond':
+        source  => 'puppet:///modules/diamond/solaris/method/diamond',
+        mode    => '0755',
+        owner   => 'root',
+        group   => 'bin',
+        require => Package['diamond'],
+      }
+      file { '/lib/svc/manifest/network/diamond.xml':
+        source  => 'puppet:///modules/diamond/solaris/manifest/diamond.xml',
+        mode    => '0444',
+        owner   => 'root',
+        group   => 'sys',
+        require => [Package['diamond'],File['/lib/svc/method/diamond']],
+      }
+    } else {
+      file { '/etc/init.d/diamond':
+        mode    => '0755',
+        require => Package['diamond'],
+      }
+    }
+    file { '/var/log/diamond':
+      ensure => directory,
     }
   } else {
-    file { '/etc/init.d/diamond':
-      mode    => '0755',
-      require => Package['diamond'],
+    package { 'diamond':
+      ensure  => $diamond::version,
     }
   }
-  file { '/var/log/diamond':
-    ensure => directory,
-  }
-} else {
-  package { 'diamond':
-    ensure  => $diamond::version,
-  }
-}
 
   file { '/var/run/diamond':
     ensure => directory,
@@ -90,13 +84,21 @@ class diamond::install {
   }
 
   if $diamond::librato_user and $diamond::librato_apikey {
-    ensure_packages(['python-pip'])
-    ensure_resource('package', 'librato-metrics', {'ensure' => 'present', 'provider' => pip, 'before' => Package['python-pip']})
+    # This package should be handled outside of our module, currently no module
+    # on the forge manages the resource so we'll let it slide
+    package { 'librato-metrics':
+      ensure   => present,
+      provider => pip,
+    }
   }
 
   if $diamond::riemann_host {
-    ensure_packages(['python-pip'])
-    ensure_resource('package', 'bernhard', {'ensure' => 'present', 'provider' => pip, 'before' => Package['python-pip']})
+    # This package should be handled outside of our module, currently no module
+    # on the forge manages the resource so we'll let it slide
+    package { 'bernhard':
+      ensure   => present,
+      provider => pip,
+    }
   }
 
 }
