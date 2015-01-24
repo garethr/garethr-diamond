095a44c011801711fa155eeec26dd135e727c360
Jared Curtis <jcurtis@xmatters.com>
2015-01-23 16:40:53 -0800

    Refactor spec tests
    Spec tests are now run in full for each supported OS
    Added support for testing diamond::python

--- spec/defines/diamond/collector_spec.rb
+++ spec/defines/diamond/collector_spec.rb
@@ -1,36 +1,46 @@
 require 'spec_helper'
 
 describe 'diamond::collector', :type => :define do
+  supported_osfamily = ['RedHat', 'Debian', 'Solaris']
+  supported_osfamily.each do |osfamily|
+    context "On #{osfamily}" do
+      osrelease = nil
+      osrelease = '5.11' if osfamily == 'Solaris'
+      let(:facts) {{
+        :osfamily               => osfamily,
+        :operatingsystemrelease => osrelease,
+      }}
 
-  context 'no parameters for testCollector' do
-    let (:title) { 'testCollector' }
+      describe 'no parameters for testCollector' do
+        let (:title) { 'testCollector' }
 
-    it { should contain_class('diamond') }
-    it { should contain_file('/etc/diamond/collectors/testCollector.conf').with_content(/^enabled=True$/) }
+        it { should contain_class('diamond') }
+        it { should contain_file('/etc/diamond/collectors/testCollector.conf').with_content(/^enabled=True$/) }
+      end
+
+      describe 'with options' do
+        let (:title) { 'testCollector' }
+        let (:params) {{
+          :options => {
+              'testOption' => 'testValue',
+            }
+        }}
+
+        it { should contain_file('/etc/diamond/collectors/testCollector.conf').with_content(/^enabled=True\ntestOption = testValue$/) }
+      end
+
+      describe 'with sections' do
+        let (:title) { 'testCollector' }
+        let (:params) {{
+          :sections => {
+              'testSection' => {
+                'testSectionOption' => 'testSectionValue',
+              }
+            }
+        }}
+
+        it { should contain_file('/etc/diamond/collectors/testCollector.conf').with_content(/^enabled=True\n\ntestSection\ntestSectionOption = testSectionValue$/) }
+      end
+    end
   end
-
-  context 'with options' do
-    let (:title) { 'testCollector' }
-    let (:params) {{
-       :options => {
-          'testOption' => 'testValue',
-        }
-    }}
-
-    it { should contain_file('/etc/diamond/collectors/testCollector.conf').with_content(/^enabled=True\ntestOption = testValue$/) }
-  end
-
-  context 'with sections' do
-    let (:title) { 'testCollector' }
-    let (:params) {{
-       :sections => {
-          'testSection' => {
-            'testSectionOption' => 'testSectionValue',
-          }
-        }
-    }}
-
-    it { should contain_file('/etc/diamond/collectors/testCollector.conf').with_content(/^enabled=True\n\ntestSection\ntestSectionOption = testSectionValue$/) }
-  end
-
 end
