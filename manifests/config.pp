03187c071ab2137a04627b48b33656b315d2abae
Steve Teuber <steve.existenz@gmail.com>
2016-04-18 10:06:04 +0200

    Include $diamond::extra_handlers variable in diamond::config
    Explicitly pull extra_handlers variable into diamond::config so that the erb template will have them in scope.

--- manifests/config.pp
+++ manifests/config.pp
@@ -23,6 +23,7 @@ class diamond::config {
   $hostname_method   = $diamond::hostname_method
   $handlers_path     = $diamond::handlers_path
   $rotate_days       = $diamond::rotate_days
+  $extra_handlers    = $diamond::extra_handlers
   file { '/etc/diamond/diamond.conf':
     ensure  => present,
     content => template('diamond/etc/diamond/diamond.conf.erb'),
